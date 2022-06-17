e464a1f0f5a96cb5183d9ca30e43b96ea56423cf
Eric Cheng <wxyz41055@gmail.com>
2022-06-17 16:29:15 +0800

    try auto deploy

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.17
 require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
-	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/joho/godotenv v1.4.0
 	github.com/line/line-bot-sdk-go/v7 v7.16.0
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -13,5 +13,4 @@ require (
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
 	golang.org/x/net v0.0.0-20220615171555-694bf12d69de // indirect
 	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- main.go
+++ main.go
@@ -1,17 +1,8 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/base64"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
@@ -29,7 +20,7 @@ func main() {
 	fmt.Println("port ", port)
 
 	bot, err := linebot.New(secret, token)
-	log.Println("Bot:", bot, " err:", err)
+	fmt.Println("Bot:", bot, " err:", err)
 	router := gin.Default()
 	router.POST("/callback", callbackHandler)
 
@@ -37,9 +28,8 @@ func main() {
 }
 
 func callbackHandler(c *gin.Context) {
-	events, err := ParseRequest1(c.Request)
-	fmt.Println("env= ", events)
-	fmt.Println("err= ", err)
+	events, err := bot.ParseRequest(c.Request)
+	fmt.Println("e", events, err)
 	if err != nil {
 		if err == linebot.ErrInvalidSignature {
 			c.String(400, "123")
@@ -51,76 +41,4 @@ func callbackHandler(c *gin.Context) {
 	for _, event := range events {
 		fmt.Println("event= ", event)
 	}
-
-	for _, event := range events {
-		if event.Type == linebot.EventTypeMessage {
-			switch message := event.Message.(type) {
-			// Handle only on text message
-			case *linebot.TextMessage:
-				quota, err := bot.GetMessageQuota().Do()
-				if err != nil {
-					log.Println("Quota err:", err)
-				}
-				// message.ID: Msg unique ID
-				// message.Text: Msg text
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("msg ID:"+message.ID+":"+"Get:"+message.Text+" , \n OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
-					log.Print(err)
-				}
-
-			// Handle only on Sticker message
-			case *linebot.StickerMessage:
-				var kw string
-				for _, k := range message.Keywords {
-					kw = kw + "," + k
-				}
-
-				outStickerResult := fmt.Sprintf("收到貼圖訊息: %s, pkg: %s kw: %s  text: %s", message.StickerID, message.PackageID, kw, message.Text)
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(outStickerResult)).Do(); err != nil {
-					log.Print(err)
-				}
-			}
-		}
-	}
-}
-
-func ParseRequest1(r *http.Request) ([]*linebot.Event, error) {
-	return ParseRequest(os.Getenv("SECRET"), r)
-}
-
-func ParseRequest(channelSecret string, r *http.Request) ([]*linebot.Event, error) {
-	defer r.Body.Close()
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Println("1")
-		return nil, err
-	}
-	if !validateSignature(channelSecret, r.Header.Get("X-Line-Signature"), body) {
-		fmt.Println("2")
-		return nil, errors.New("ErrInvalidSignature")
-	}
-
-	request := &struct {
-		Events []*linebot.Event `json:"events"`
-	}{}
-	if err = json.Unmarshal(body, request); err != nil {
-		fmt.Println("3")
-		return nil, err
-	}
-	fmt.Println("done")
-	return request.Events, nil
-}
-
-func validateSignature(channelSecret, signature string, body []byte) bool {
-	decoded, err := base64.StdEncoding.DecodeString(signature)
-	if err != nil {
-		return false
-	}
-	hash := hmac.New(sha256.New, []byte(channelSecret))
-
-	_, err = hash.Write(body)
-	if err != nil {
-		return false
-	}
-
-	return hmac.Equal(decoded, hash.Sum(nil))
 }
